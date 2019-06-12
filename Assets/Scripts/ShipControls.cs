605502916c36981b369868e1a740197202118cb4
Sarah Elwise <sleedy13@gmail.com>
2019-06-11 21:50:58 -0400

    Update ShipControls.cs
    Basic control of player ship, need to sort out how to handle rotation

--- Assets/Scripts/ShipControls.cs
+++ Assets/Scripts/ShipControls.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ShipControls : MonoBehaviour
 {
+    Rigidbody2D rigidbody2D;
+    float rotation;
+    float speedLimit;
+    float acceleration=0.1f;
+    public int maxHealth = 100;
+    int currentHealth;
+    public int Health
+    {
+        get {return currentHealth;}
+        set {currentHealth = value;}
+    }
+    float velocity = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float vertical = Input.GetAxis("Vertical");
+
+        if (vertical > 0) velocity += acceleration;
+        else if (vertical < 0) velocity -= acceleration;
+
+        Vector2 position = rigidbody2D.position;
+        position.y += velocity * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
+
     }
 }
