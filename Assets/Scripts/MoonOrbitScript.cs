02ea5fb817f299a9a835332ccfecd2c69eaa446e
Sarah Elwise <sleedy13@gmail.com>
2019-06-18 07:22:59 -0400

    Added in a moon object and script, moon can orbit around any object

---
+++ Assets/Scripts/MoonOrbitScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonOrbitScript : MonoBehaviour
+{
+    public float speed;
+    public GameObject originatorPlanet;
+    private Vector3 center;
+
+    private void Start()
+    {
+        center = originatorPlanet.transform.position;
+    }
+
+    void Update()
+    {
+        transform.RotateAround(center,transform.forward, speed*Time.deltaTime);
+    }
+}
